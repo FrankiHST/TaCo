fd357551ab0dbae8dd1cbd7fce16fc21d635f95b
Frank Schoch <Frank.Schoch@sva.de>
2024-03-13 17:59:05 +0100

    separieren der Config Zeilen

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ After this generates configuration files for Theobald XTract Unsiversal.
 import json
 import sys
 
-def read_config(path):
+def read_config_to_string(path):
     """
     That´s a method to read the coinfig file. 
     """
@@ -16,19 +16,21 @@ def read_config(path):
         print(obj)
         return(obj)
 
-def single_items(json_string):
+def strings_by_extractiontype(json_string):
     """
     This method splits the json in single objects.
     """
-    string = json_string
-    print(string)
+    number_of_extractiontypes = len(json_string["tables"])
+    for x in range(number_of_extractiontypes):
+        extractiontype_line=json_string["tables"][x]["table_name"]
+        print(extractiontype_line)
 
 def main():
     """Main method.
     """
     config_path = "./config.json"
-    file = read_config(config_path)
-    single_items(file)
+    file_string = read_config_to_string(config_path)
+    strings_by_extractiontype(file_string)
 
 if __name__ == "__main__":
     sys.exit(main())
