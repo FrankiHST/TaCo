b7ac512c3a8126f9fcbebb5b5a4fa4ea886a5fe0
Frank Schoch <Frank.Schoch@sva.de>
2024-03-14 10:03:12 +0100

    dynamic in Selngle Lines

--- start.py
+++ start.py
@@ -12,25 +12,26 @@ def read_config_to_string(path):
     """
     with open(path,'r',encoding="utf-8") as file:
         obj = json.load(file)
-        #print(len(obj['tables']))
-        print(obj)
         return(obj)
 
-def strings_by_extractiontype(json_string):
+def extraction_data(json_string, extraction_type):
     """
     This method splits the json in single objects.
     """
-    number_of_extractiontypes = len(json_string["tables"])
+    number_of_extractiontypes = len(json_string[extraction_type])
+    extractiontype_lines_list=[]
     for x in range(number_of_extractiontypes):
-        extractiontype_line=json_string["tables"][x]["table_name"]
-        print(extractiontype_line)
+        extractiontype_line = json_string[extraction_type][x][extraction_type+"_name"]
+        extractiontype_lines_list.append(extractiontype_line)
+    print(extractiontype_lines_list)
 
 def main():
     """Main method.
     """
+    x_type = "table"
     config_path = "./config.json"
-    file_string = read_config_to_string(config_path)
-    strings_by_extractiontype(file_string)
+    x_string = read_config_to_string(config_path)
+    extraction_data(x_string, x_type)
 
 if __name__ == "__main__":
     sys.exit(main())
