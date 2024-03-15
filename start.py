640b3afe2fc582072b4c23750f8b5dc284488787
Frank Schoch <Frank.Schoch@sva.de>
2024-03-15 10:24:55 +0100

    eigene Klassen erstellen

--- start.py
+++ start.py
@@ -6,33 +6,53 @@ After this generates configuration files for Theobald XTract Unsiversal.
 import json
 import sys
 
+class Extraktion:
+    """
+    Dies ist die Klasse Extraktion.
+    https://www.hdm-stuttgart.de/~maucher/Python/html/Klassen.html
+    Sie entspricht einer konfigurierten Extraktion im XTract Universal.
+    """
+    angelegteExtraktionen=0
+    def __init__(self,extraktionsnamensuffix="V001",extraktionstyp="table"):
+        self.__extraktionsnamensuffix=extraktionsnamensuffix
+        self.__extraktionstyp=extraktionstyp
+        Extraktion.angelegteExtraktionen+=1
+
+    def __del__(self):
+        Extraktion.angelegteExtraktionen-=1
+
+
 def read_config_to_string(path):
     """
     That´s a method to read the coinfig file. 
     """
     with open(path,'r',encoding="utf-8") as file:
         obj = json.load(file)
-        return(obj)
+        return obj
 
-def extraction_data(json_string, extraction_type):
+def table_extraction_data(json_string, extraction_type):
     """
     This method splits the json in single objects.
     """
     number_of_extractiontypes = len(json_string[extraction_type])
-    extractiontype_lines_list={}
+    extraction_lines_list={}
     for x in range(number_of_extractiontypes):
-        extractiontype_list_table_name=json_string[extraction_type][x][extraction_type+"_name"]
-        for y, extractiontype_list_table_name in enumerate(extractiontype_list_table_name):
+        extraction_list_table_name=json_string[extraction_type][x][extraction_type+"_name"]
+        for y, extraction_list_table_name in enumerate(extraction_list_table_name):
             extractiontype_lines=[]
-            extractiontype_lines.append(json_string[extraction_type][x][extraction_type+"_name"])
+            extractiontype_lines.append(json_string[extraction_type][x][extraction_type+"_name"][y])
             extractiontype_lines.append(json_string[extraction_type][x]["source"])
             extractiontype_lines.append(json_string[extraction_type][x]["dest"])
             extractiontype_lines.append(json_string[extraction_type][x]["dest_type"])
-            extractiontype_lines.append(json_string[extraction_type][x]["name_suffix"])
-            print(extractiontype_list_table_name)
-            extraction_name=json_string[extraction_type][x]["source"]+'_'+extractiontype_list_table_name[y]+'_'+json_string[extraction_type][x]["dest"]+'_'+json_string[extraction_type][x]["name_suffix"]
-            extractiontype_lines_list[extraction_name]=extractiontype_lines
-    print(extractiontype_lines_list)
+            extractiontype_lines.append(json_string[extraction_type][x]["machine"])
+            extractiontype_lines.append("Table")
+            print(extraction_list_table_name)
+            extraction_name=(
+                json_string[extraction_type][x]["source"]+'_'+
+                extraction_list_table_name+'_'+json_string[extraction_type][x]["dest"]+'_'+
+                json_string[extraction_type][x]["name_suffix"])
+            extraction_lines_list[extraction_name]=extractiontype_lines
+    print(extraction_lines_list)
 
 def main():
     """Main method.
@@ -40,7 +60,7 @@ def main():
     x_type = "table"
     config_path = "./config.json"
     x_string = read_config_to_string(config_path)
-    extraction_data(x_string, x_type)
+    table_extraction_data(x_string, x_type)
 
 if __name__ == "__main__":
     sys.exit(main())
